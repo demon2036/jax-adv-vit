277acedcb1bc00ba29860d83a6ef214cd911363c
jtitor <1165137798@qq.com>
2024-05-22 03:25:48 +0800

    commit

--- test.py
+++ test.py
@@ -17,9 +17,9 @@
 Library file which executes the training and evaluation loop for MNIST.
 The data is loaded using tensorflow_datasets.
 """
+from dataset import create_dataloaders
 from main import get_train_dataloader
 import jax
-
 
 jax.distributed.initialize()
 
@@ -40,7 +40,70 @@ EPSILON = 8 / 255  # @param{type:"number"}
 # os.environ['WANDB_API_KEY'] = 'ec6aa52f09f51468ca407c0c00e136aaaa18a445'
 
 if __name__ == "__main__":
-    _, train_dataloader, test_dataloader = get_train_dataloader(TRAIN_BATCH_SIZE)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train-dataset-shards")
+    parser.add_argument("--valid-dataset-shards")
+    parser.add_argument("--train-batch-size", type=int, default=2048)
+    parser.add_argument("--valid-batch-size", type=int, default=256)
+    parser.add_argument("--train-loader-workers", type=int, default=40)
+    parser.add_argument("--valid-loader-workers", type=int, default=5)
+
+    parser.add_argument("--random-crop", default="rrc")
+    parser.add_argument("--color-jitter", type=float, default=0.0)
+    parser.add_argument("--auto-augment", default="rand-m9-mstd0.5-inc1")
+    parser.add_argument("--random-erasing", type=float, default=0.25)
+    parser.add_argument("--augment-repeats", type=int, default=3)
+    parser.add_argument("--test-crop-ratio", type=float, default=0.875)
+
+    parser.add_argument("--mixup", type=float, default=0.8)
+    parser.add_argument("--cutmix", type=float, default=1.0)
+    parser.add_argument("--criterion", default="ce")
+    parser.add_argument("--label-smoothing", type=float, default=0.1)
+
+    parser.add_argument("--layers", type=int, default=12)
+    parser.add_argument("--dim", type=int, default=768)
+    parser.add_argument("--heads", type=int, default=12)
+    parser.add_argument("--labels", type=int, default=-1)
+    parser.add_argument("--layerscale", action="store_true", default=False)
+    parser.add_argument("--patch-size", type=int, default=16)
+    parser.add_argument("--image-size", type=int, default=224)
+    parser.add_argument("--posemb", default="learnable")
+    parser.add_argument("--pooling", default="cls")
+    parser.add_argument("--dropout", type=float, default=0.0)
+    parser.add_argument("--droppath", type=float, default=0.1)
+    parser.add_argument("--grad-ckpt", action="store_true", default=False)
+
+    parser.add_argument("--init-seed", type=int, default=random.randint(0, 1000000))
+    parser.add_argument("--mixup-seed", type=int, default=random.randint(0, 1000000))
+    parser.add_argument("--dropout-seed", type=int, default=random.randint(0, 1000000))
+    parser.add_argument("--shuffle-seed", type=int, default=random.randint(0, 1000000))
+    parser.add_argument("--pretrained-ckpt")
+    parser.add_argument("--label-mapping")
+
+    parser.add_argument("--optimizer", default="adamw")
+    parser.add_argument("--learning-rate", type=float, default=1e-3)
+    parser.add_argument("--weight-decay", type=float, default=0.05)
+    parser.add_argument("--adam-b1", type=float, default=0.9)
+    parser.add_argument("--adam-b2", type=float, default=0.999)
+    parser.add_argument("--adam-eps", type=float, default=1e-8)
+    parser.add_argument("--lr-decay", type=float, default=1.0)
+    parser.add_argument("--clip-grad", type=float, default=0.0)
+    parser.add_argument("--grad-accum", type=int, default=1)
+    parser.add_argument("--use-kan", action="store_true", default=False)
+    parser.add_argument("--polynomial-degree", type=int, default=8)
+
+    parser.add_argument("--warmup-steps", type=int, default=10000)
+    parser.add_argument("--training-steps", type=int, default=200000)
+    parser.add_argument("--log-interval", type=int, default=50)
+    parser.add_argument("--eval-interval", type=int, default=0)
+
+    parser.add_argument("--project")
+    parser.add_argument("--name")
+    parser.add_argument("--ipaddr")
+    parser.add_argument("--hostname")
+    parser.add_argument("--output-dir", default=".")
+
+    train_dataloader, valid_dataloader = create_dataloaders(parser.parse_args())
     train_dataloader_iter = iter(train_dataloader)
 
     for _ in range(100):
