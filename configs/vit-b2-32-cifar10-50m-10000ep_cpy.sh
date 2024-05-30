2305cc30c785339375e48ccfbf514f1ffcdb192f
jtitor <1165137798@qq.com>
2024-05-30 23:07:13 +0800

    commit

--- configs/vit-b2-32-cifar10-50m-10000ep_cpy.sh
+++ configs/vit-b2-32-cifar10-50m-10000ep_cpy.sh
@@ -2,6 +2,8 @@ export EPOCH=10000 TRAIN_BATCH_SIZE=1024 WARMUP_EPOCH=100  GCS_DATASET_DIR=gs://
 
 
 python -u main_copy.py \
+    --warmup-steps $((50000 * $WARMUP_EPOCH / $TRAIN_BATCH_SIZE)) \
+    --training-steps $((50000 * $EPOCH / $TRAIN_BATCH_SIZE)) \
     --train-dataset-shards "$GCS_DATASET_DIR/cifar10-50m-wds/shards-{00000..00806}.tar" \
     --valid-dataset-shards  "gs://caster-us-central-2b/cifar10-test-wds/shards-{00000..00078}.tar" \
     --layers 12  \
@@ -18,8 +20,6 @@ python -u main_copy.py \
     --train-batch-size $TRAIN_BATCH_SIZE \
     --learning-rate 1e-4 \
     --weight-decay 0.5 \
-    --warmup-steps $((50000 * $WARMUP_EPOCH / $TRAIN_BATCH_SIZE)) \
-    --training-steps $((50000 * $EPOCH / $TRAIN_BATCH_SIZE)) \
     --project cifar10-20m \
     --name $(basename $0 .sh) \
     --ema-decay 0.99996
