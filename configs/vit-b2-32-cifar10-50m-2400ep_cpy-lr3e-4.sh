01d491b0daf926975e2f0334355e87d28cb706bc
jtitor <1165137798@qq.com>
2024-06-01 08:27:00 +0800

    commit

---
+++ configs/vit-b2-32-cifar10-50m-2400ep_cpy-lr3e-4.sh
@@ -0,0 +1,24 @@
+export EPOCH=2400 TRAIN_BATCH_SIZE=1024 WARMUP_EPOCH=5  GCS_DATASET_DIR=gs://caster-us-central-2b
+
+
+python -u main_copy.py \
+    --train-dataset-shards "$GCS_DATASET_DIR/cifar10-50m-wds/shards-{00000..00806}.tar" \
+    --valid-dataset-shards  "gs://caster-us-central-2b/cifar10-test-wds/shards-{00000..00078}.tar" \
+    --layers 12  \
+    --dim 768  \
+    --heads 12  \
+    --labels 10  \
+    --layerscale   \
+    --patch-size 2  \
+    --image-size 32  \
+    --posemb "learnable"  \
+    --pooling 'cls'  \
+    --dropout 0.0  \
+    --droppath 0.0  \
+    --train-batch-size $TRAIN_BATCH_SIZE \
+    --learning-rate 3.0e-4 \
+    --weight-decay 0.2 \
+    --warmup-steps $((50000 * $WARMUP_EPOCH / $TRAIN_BATCH_SIZE)) \
+    --training-steps $((50000 * $EPOCH / $TRAIN_BATCH_SIZE)) \
+    --project cifar10-20m \
+    --name $(basename $0 .sh) \
