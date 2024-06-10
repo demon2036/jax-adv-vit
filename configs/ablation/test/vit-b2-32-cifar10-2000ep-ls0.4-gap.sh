5fbd2603bd6c2078ece5f4789e2673d81d75ff39
jtitor <1165137798@qq.com>
2024-06-10 18:40:33 +0800

    commit

--- configs/ablation/test/vit-b2-32-cifar10-2000ep-ls0.4-gap.sh
+++ configs/ablation/test/vit-b2-32-cifar10-2000ep-ls0.4-gap.sh
@@ -21,7 +21,7 @@ python -u main_copy_fork.py \
     --weight-decay 0.5 \
     --warmup-steps $((50000 * $WARMUP_EPOCH / $TRAIN_BATCH_SIZE)) \
     --training-steps $((50000 * $EPOCH / $TRAIN_BATCH_SIZE)) \
-    --project cifar10-ablation-test \
+    --project cifar10-ablation-test2 \
     --name $(basename $0 .sh) \
     --output-dir "$GCS_DATASET_DIR/ablation/test" \
     --beta 5.0 \
