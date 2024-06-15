5873d76bfd464ed29a14b313bb36c4b8d687f968
jtitor <1165137798@qq.com>
2024-06-15 18:52:03 +0800

    commit

--- configs/mae/pretrain/mae-vit-b2-32-cifar10-2000ep-batch-16384.sh
+++ configs/mae/pretrain/mae-vit-b2-32-cifar10-2000ep-batch-16384.sh
@@ -27,7 +27,7 @@ python3 src/main_pretrain_mae.py \
     --dropout-seed 0 \
     --shuffle-seed 0 \
     --optimizer lamb \
-    --learning-rate 2.4e-3 \
+    --learning-rate 5e-3 \
     --weight-decay 0.05 \
     --adam-b1 0.9 \
     --adam-b2 0.95 \
