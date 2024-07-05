ffaa64a099cd6460198fec808d9139caf1724329
jtitor <1165137798@qq.com>
2024-07-05 19:35:38 +0800

    commit

--- configs/best/cifar10/vit-h2-32-cifar10-10000ep-ls0.4-gap-beta3-test.sh
+++ configs/best/cifar10/vit-h2-32-cifar10-10000ep-ls0.4-gap-beta3-test.sh
@@ -1,7 +1,7 @@
 export EPOCH=10000 TRAIN_BATCH_SIZE=1024 WARMUP_EPOCH=200
 
 
-python -u main_copy_fork_cpy.py \
+python -u main_copy_fork_register.py \
     --train-dataset-shards "$GCS_DATASET_DIR/cifar10-50m-wds/shards-{00000..00999}.tar" \
     --valid-dataset-shards  "$GCS_DATASET_DIR/cifar10-test-wds/shards-{00000..00099}.tar" \
     --train-origin-dataset-shards "$GCS_DATASET_DIR/cifar10-train-wds/shards-{00000..00099}.tar" \
