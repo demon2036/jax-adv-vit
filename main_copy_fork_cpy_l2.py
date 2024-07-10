06b181d79cbef7637ec3fb8dc6b8e6bd8e38633f
jtitor <1165137798@qq.com>
2024-07-10 08:49:52 +0800

    commit

--- main_copy_fork_cpy_l2.py
+++ main_copy_fork_cpy_l2.py
@@ -254,6 +254,7 @@ def train_and_evaluate(args
 
     if jax.process_index() == 0:
         wandb.init(name=args.name, project=args.project, config=args.__dict__,
+                   settings=wandb.Settings(_disable_stats=True),
                    config_exclude_keys=['train_dataset_shards', 'valid_dataset_shards', 'train_origin_dataset_shards'])
         average_meter = AverageMeter(use_latest=["learning_rate"])
 
