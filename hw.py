32170b8f6c3d39081d632685d826b658924c3c4e
jtitor <1165137798@qq.com>
2024-07-12 15:42:26 +0800

    commit

--- hw.py
+++ hw.py
@@ -364,14 +364,21 @@ def train_and_evaluate(args
         erase_and_create_empty(filename)
         save_data = flax.jax_utils.unreplicate(state)
 
+        orbax_checkpointer = ocp.PyTreeCheckpointer()
+        options = ocp.CheckpointManagerOptions(max_to_keep=1, create=True)
+        checkpoint_manager = ocp.CheckpointManager(
+            filename, orbax_checkpointer, options)
+
+
         model_ckpt = {'model': save_data, }
         save_args = orbax_utils.save_args_from_target(model_ckpt)
 
         print(filename)
         print(1)
-        orbax_checkpointer = ocp.PyTreeCheckpointer()
+        # orbax_checkpointer = ocp.PyTreeCheckpointer()
         # save_args = orbax_utils.save_args_from_target(save_data)
-        orbax_checkpointer.save(filename, model_ckpt, save_args=save_args, force=False)
+        # orbax_checkpointer.save(filename, model_ckpt, save_args=save_args, force=False)
+        checkpoint_manager.save(1, model_ckpt, save_kwargs={'save_args': save_args}, force=False)
 
         print(2)
     # checkpointer.save(filename, args=ocp.args.StandardSave(save_data),
