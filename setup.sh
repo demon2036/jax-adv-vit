0e264e09c459ee538a3000097dce4ea28f3ef6bb
jtitor <1165137798@qq.com>
2024-07-12 12:19:53 +0800

    commit

--- setup.sh
+++ setup.sh
@@ -26,7 +26,7 @@ eval "$(~/miniconda3/bin/conda shell.bash hook)"
 pip install -U jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install -U torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 pip install -U flax optax chex webdataset timm wandb
-pip install einops
+pip install einops gcsfs
 
 
 # 3. Install Pillow-SIMD.
