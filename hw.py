81b2409ba16fa73bfc1d2ce11c6e34bca5938149
jtitor <1165137798@qq.com>
2024-07-12 16:35:19 +0800

    commit

--- hw.py
+++ hw.py
@@ -1,62 +1,25 @@
-from typing import Optional, Any
-import shutil
-
 import numpy as np
+import orbax.checkpoint as ocp
 import jax
-from jax import random, numpy as jnp
-import os
-import flax
-from flax import linen as nn
-from flax.training import checkpoints, train_state
-from flax import struct, serialization
-import orbax.checkpoint
 
-import optax
+path = ocp.test_utils.erase_and_create_empty('/tmp/my-checkpoints/')
 
 
-ckpt_dir = '/tmp/flax_ckpt'
-
-if os.path.exists(ckpt_dir):
-    shutil.rmtree(ckpt_dir)  # Remove any existing checkpoints from the last notebook run.
-
-# A simple model with one linear layer.
-key1, key2 = random.split(random.key(0))
-x1 = random.normal(key1, (5,))      # A simple JAX array.
-model = nn.Dense(features=3)
-variables = model.init(key2, x1)
-
-# Flax's TrainState is a pytree dataclass and is supported in checkpointing.
-# Define your class with `@flax.struct.dataclass` decorator to make it compatible.
-tx = optax.sgd(learning_rate=0.001)      # An Optax SGD optimizer.
-state = train_state.TrainState.create(
-    apply_fn=model.apply,
-    params=variables['params'],
-    tx=tx)
-# Perform a simple gradient update similar to the one during a normal training workflow.
-state = state.apply_gradients(grads=jax.tree_util.tree_map(jnp.ones_like, state.params))
-
-# Some arbitrary nested pytree with a dictionary and a NumPy array.
-config = {'dimensions': np.array([5, 3])}
-
-# Bundle everything together.
-ckpt = {'model': state, 'config': config, 'data': [x1]}
+my_tree = {
+    'a': np.arange(8),
+    'b': {
+        'c': 42,
+        'd': np.arange(16),
+    },
+}
+abstract_my_tree = jax.tree_util.tree_map(
+    ocp.utils.to_shape_dtype_struct, my_tree)
 
 
-
-from flax.training import orbax_utils
-
-orbax_checkpointer = orbax.checkpoint.PyTreeCheckpointer()
-save_args = orbax_utils.save_args_from_target(ckpt)
-orbax_checkpointer.save('/tmp/flax_ckpt/orbax/single_save', ckpt, save_args=save_args)
-
-
-options = orbax.checkpoint.CheckpointManagerOptions(max_to_keep=2, create=True)
-checkpoint_manager = orbax.checkpoint.CheckpointManager(
-    '/tmp/flax_ckpt/orbax/managed', orbax_checkpointer, options)
-
-# Inside a training loop
-for step in range(5):
-    # ... do your training
-    checkpoint_manager.save(step, ckpt, save_kwargs={'save_args': save_args})
-
-os.listdir('/tmp/flax_ckpt/orbax/managed')  # Because max_to_keep=2, only step 3 and 4 are retained
+checkpointer = ocp.StandardCheckpointer()
+# 'checkpoint_name' must not already exist.
+checkpointer.save(path / 'checkpoint_name', my_tree)
+print(checkpointer.restore(
+    path / 'checkpoint_name/',
+    args=ocp.args.StandardRestore(abstract_my_tree)
+))
