954896a3dc3ece847fa46294a74c11b4c1aef5b9
jtitor <1165137798@qq.com>
2024-07-26 00:16:13 +0800

    commit

--- configs/test/vit-b2-32-cifar10-2000ep-ls0.4-gap-beta3-mp.sh
+++ configs/test/vit-b2-32-cifar10-2000ep-ls0.4-gap-beta3-mp.sh
@@ -13,17 +13,20 @@ python -u main_copy_fork.py \
     --patch-size 2  \
     --image-size 32  \
     --posemb "learnable"  \
-    --pooling 'gap'  \
+    --pooling gap  \
     --dropout 0.0  \
     --droppath 0.0  \
     --train-batch-size $TRAIN_BATCH_SIZE \
     --learning-rate 1e-4 \
     --weight-decay 0.5 \
+    --adam-b1 0.9 \
+    --adam-b2 0.99 \
+    --clip-grad 1.0 \
     --warmup-steps $((50000 * $WARMUP_EPOCH / $TRAIN_BATCH_SIZE)) \
     --training-steps $((50000 * $EPOCH / $TRAIN_BATCH_SIZE)) \
-     --eval-interval $((50000 * 50 / $TRAIN_BATCH_SIZE)) \
-    --project cifar1000-ablation-ls \
+    --eval-interval $((50000 * 50 / $TRAIN_BATCH_SIZE)) \
+    --project cifar1000-ablation-beta-new \
     --name $(basename $0 .sh) \
-    --output-dir "$GCS_DATASET_DIR/ablation/mp" \
-    --beta 5.0 \
-    --label-smoothing 0.3
+    --output-dir "$GCS_DATASET_DIR/ablation/beta" \
+    --beta 3.0 \
+    --label-smoothing 0.4
