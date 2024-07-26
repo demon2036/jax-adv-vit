f71a8fdca3d582ebafe079c93d52f95dff3e77fe
jtitor <1165137798@qq.com>
2024-07-26 19:43:23 +0800

    commit

--- model.py
+++ model.py
@@ -162,10 +162,11 @@ class Attention(ViTBase, nn.Module):
         y = self.to_qkv(x)  # b l d
 
         y = einops.rearrange(y, 'b l (h d v) -> b h d v l  ', h=self.heads, v=3)
-        # y = y.reshape(y.shape[0], self.heads, -1, 3, y.shape[1])
-        q, k, v = jnp.split(normalize_jax(y, dim=2), 3, 3)  # pixel norm & split
+        y = normalize_jax(y, dim=2)
 
-        print(q.shape,k.shape,v.shape)
+        q, k, v = einops.rearrange(y, ' b h d v  ->v b h d  l  ', )
+
+        print(q.shape, k.shape, v.shape)
 
         w = jnp.einsum('nhcq,nhck->nhqk', q, k / np.sqrt(q.shape[2]))
         w = nn.softmax(w, axis=3)
