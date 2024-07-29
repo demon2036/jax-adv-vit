7150e5fda917480626de5d4033576e1290980e14
jtitor <1165137798@qq.com>
2024-07-29 22:08:14 +0800

    commit

--- test_host.py
+++ test_host.py
@@ -1,5 +1,20 @@
 import jax
+import flax
+import jax.numpy as jnp
+from jax.experimental import mesh_utils
+from jax.sharding import Mesh, PartitionSpec, NamedSharding
+
+
+def case1():
+    device_mesh = mesh_utils.create_device_mesh((2, 4))
+    if jax.process_index() == 0:
+        print(device_mesh)
+
 
 if __name__ == "__main__":
     jax.distributed.initialize()
-    print(jax.devices())
+
+    if jax.process_index() == 0:
+        print(jax.devices())
+
+    case1()
