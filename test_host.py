99aea07056791b2fd7dc5e0207f0b4954a5ac91d
jtitor <1165137798@qq.com>
2024-07-30 11:14:58 +0800

    commit

--- test_host.py
+++ test_host.py
@@ -13,6 +13,10 @@ from jax.sharding import Mesh, PartitionSpec, NamedSharding
 import flax.linen as nn
 
 
+def block_all(xs):
+    jax.tree_util.tree_map(lambda x: x.block_until_ready(), xs)
+    return xs
+
 def case1():
     device_mesh = mesh_utils.create_device_mesh((jax.device_count(),))
     mesh = Mesh(device_mesh, axis_names=('data',))
@@ -49,7 +53,7 @@ def case1():
     # )
     """"""
 
-    global_batch_array=jax.device_put(x,x_sharding)
+    global_batch_array = jax.device_put(x, x_sharding)
 
     rng = jax.random.PRNGKey(1)
     model = DPDense(384)
@@ -90,9 +94,7 @@ def case1():
     # end = time.time()
     # print(end - start)
 
-    def block_all(xs):
-        jax.tree_util.tree_map(lambda x: x.block_until_ready(), xs)
-        return xs
+
 
     with mesh:
 
@@ -119,6 +121,95 @@ def case1():
             # jax.debug.visualize_array_sharding(params['Dense_0']['kernel'])
             # print(global_batch_array.shape)
             print(end - start)
+
+
+def case3():
+    device_mesh = mesh_utils.create_device_mesh((jax.device_count(),))
+    print(device_mesh)
+
+    mesh = Mesh(devices=device_mesh, axis_names=('data',))
+    print(mesh)
+
+    def mesh_sharding(pspec: PartitionSpec) -> NamedSharding:
+        return NamedSharding(mesh, pspec)
+
+    class DPDense(nn.Module):
+        dim: int = 384
+
+        @nn.compact
+        def __call__(self, x, *args, **kwargs):
+            for i in range(12):
+                x = nn.Dense(self.dim, )(x)
+
+            x = jax.lax.with_sharding_constraint(x, mesh_sharding(PartitionSpec('data', )))
+            return x
+
+    shape = (128, 256, 384)
+    x = jnp.ones(shape)
+    x_sharding = mesh_sharding(PartitionSpec('data'))
+    x = jax.device_put(x, x_sharding)
+    model=DPDense()
+    rng = jax.random.PRNGKey(1)
+
+    def init_fn( k,x, model):
+        variables = model.init(k, x)  # Initialize the model.
+        return variables['params']
+
+    abstract_variables = jax.eval_shape(
+        functools.partial(init_fn, model=model,), rng, x)
+
+    # This `state_sharding` has the same pytree structure as `state`, the output
+    # of the `init_fn`.
+    state_sharding = nn.get_sharding(abstract_variables, mesh)
+
+    jit_init_fn = jax.jit(init_fn, static_argnums=(2,),
+                          in_shardings=(mesh_sharding(()), x_sharding),  # PRNG key and x
+                          out_shardings=state_sharding)
+
+    initialized_params = jit_init_fn(rng, x, model)
+
+    @functools.partial(jax.jit, in_shardings=(x_sharding,state_sharding, ),
+                       out_shardings=state_sharding)
+    def train_step( x,params):
+        # A fake loss function.
+        def loss_unrolled(params):
+            y = model.apply({'params': params}, x)
+            return y.sum()
+
+        grad_fn = jax.grad(loss_unrolled)
+        grads = grad_fn(params)
+        # state = state.apply_gradients(grads=grads)
+        return grads
+
+    with mesh:
+
+        params = block_all(train_step(x, params))
+
+        for i in range(100):
+            params = block_all(train_step(x, params))
+
+        start = time.time()
+        for i in range(1000):
+            params = block_all(train_step(x, params))
+        end = time.time()
+
+        if jax.process_index() == 0:
+            print(device_mesh)
+            print(x_sharding.addressable_devices)
+            # print()
+            # print(mesh)
+            # jax.debug.visualize_sharding((shape[0], shape[1]), sharding=x_sharding)
+            # jax.debug.visualize_array_sharding(global_batch_array[:, :, 0])
+            #
+            # print(x_sharding.addressable_devices)
+            # print(state_sharding)
+            # jax.debug.visualize_array_sharding(params['Dense_0']['kernel'])
+            # print(global_batch_array.shape)
+            print(end - start)
+
+
+
+
 
 
 def case2():
@@ -190,6 +281,6 @@ if __name__ == "__main__":
 
     if jax.process_index() == 0:
         print(jax.devices())
-
-    case1()
-    case2()
+    case3()
+    # case1()
+    # case2()
