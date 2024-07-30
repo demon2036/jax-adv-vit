c94683f89295ac21acbbe93e81aa273b77b8fa15
jtitor <1165137798@qq.com>
2024-07-30 09:06:22 +0800

    commit

--- test_host.py
+++ test_host.py
@@ -1,6 +1,7 @@
 import jax
 import flax
 import jax.numpy as jnp
+import numpy as np
 from flax.linen.linear import default_kernel_init
 from jax.experimental import mesh_utils
 from jax.sharding import Mesh, PartitionSpec, NamedSharding
@@ -29,17 +30,31 @@ def case1():
     x = jnp.ones(shape)
     x_sharding = mesh_sharding(PartitionSpec('data'))
     # x = jax.device_put(x, x_sharding)
-    x = jax.make_array_from_callback(x.shape, x_sharding, lambda idx: x[idx])
+
+    global_batch_shape=(256,256,384)
+
+    per_replica_batches = np.split(x, jax.local_device_count())
+
+
+    global_batch_array=jax.make_array_from_single_device_arrays(
+        global_batch_shape,sharding=x_sharding,
+        arrays=[
+            jax.device_put(batch,device)
+            for batch,device in zip(per_replica_batches,x_sharding.addressable_devices)
+        ]
+    )
+    """"""
+    # x = jax.make_array_from_callback(x.shape, x_sharding, lambda idx: x[idx])
 
     if jax.process_index() == 0:
 
-        print(x.shape)
+        print(global_batch_array.shape)
 
         print(device_mesh)
         print()
         print(mesh)
         jax.debug.visualize_sharding((shape[0], shape[1]), sharding=x_sharding)
-        jax.debug.visualize_array_sharding(x[:, :, 0])
+        jax.debug.visualize_array_sharding(global_batch_array[:, :, 0])
 
 
 if __name__ == "__main__":
