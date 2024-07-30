b48208e0564508dd3a3605d77ac641799fa0b418
jtitor <1165137798@qq.com>
2024-07-30 11:58:33 +0800

    commit

--- test_host.py
+++ test_host.py
@@ -38,7 +38,6 @@ def case1():
         return NamedSharding(mesh, pspec)
 
 
-
     shape = (128, 256, 384)
     x = jnp.ones(shape)
     x_sharding = mesh_sharding(PartitionSpec('data'))
@@ -121,6 +120,126 @@ def case1():
     return global_batch_array
 
 
+
+
+
+
+
+def case3():
+
+    class DPDense(nn.Module):
+        dim: int
+        precision:jax.lax.Precision=jax.lax.Precision.HIGHEST
+
+        @nn.compact
+        def __call__(self, x, *args, **kwargs):
+            for i in range(12):
+                x = nn.Dense(self.dim, precision=self.precision)(x)
+
+            return x
+
+    device_mesh = mesh_utils.create_device_mesh((jax.device_count(),))
+    mesh = Mesh(device_mesh, axis_names=('data',))
+
+    def mesh_sharding(pspec: PartitionSpec) -> NamedSharding:
+        return NamedSharding(mesh, pspec)
+
+
+    shape = (128, 256, 384)
+    x = jnp.ones(shape)
+    x_sharding = mesh_sharding(PartitionSpec('data'))
+    """
+    # x = jax.device_put(x, x_sharding)
+    global_batch_array = jax.device_put(x, x_sharding)
+    """
+    global_batch_shape = (128 * jax.process_count(), 256, 384)
+
+    per_replica_batches = np.split(x, jax.local_device_count())
+
+    global_batch_array = jax.make_array_from_single_device_arrays(
+        global_batch_shape, sharding=x_sharding,
+        arrays=[
+            jax.device_put(batch, device)
+            for batch, device in zip(per_replica_batches, x_sharding.addressable_devices)
+        ]
+    )
+
+    rng = jax.random.PRNGKey(1)
+    model = DPDense(384)
+
+    def init_fn(x, model):
+        variables = model.init(rng, x)
+        return variables['params']
+
+    abstract_variables = jax.eval_shape(
+        functools.partial(init_fn, model=model, ), global_batch_array)
+
+    state_sharding = nn.get_sharding(abstract_variables, mesh)
+
+    jit_init_fn = jax.jit(init_fn, static_argnums=(1,),
+                          in_shardings=x_sharding,  # PRNG key and x
+                          out_shardings=state_sharding)
+
+    params = jit_init_fn(global_batch_array, model)
+
+    def train_step(x, params):
+        # out = model.apply({'params': params}, x)
+        # return out
+
+        def loss_fn(params):
+            out = model.apply({'params': params}, x)
+            loss = (jnp.zeros_like(out) - out).mean()
+            return loss
+
+        grad = jax.grad(loss_fn)(params)
+
+        return grad
+
+    train_step_jit = jax.jit(train_step, in_shardings=(x_sharding, state_sharding), out_shardings=(state_sharding), )
+
+
+    with mesh:
+
+        grad = block_all(train_step_jit(global_batch_array, params))
+
+        for i in range(100):
+            grad = block_all(train_step_jit(global_batch_array, params))
+
+        start = time.time()
+        for i in range(1000):
+            grad = block_all(train_step_jit(global_batch_array, params))
+        end = time.time()
+
+        if jax.process_index() == 0:
+            print(device_mesh)
+            print(x_sharding.addressable_devices)
+            # print()
+            # print(mesh)
+            # jax.debug.visualize_sharding((shape[0], shape[1]), sharding=x_sharding)
+            # jax.debug.visualize_array_sharding(global_batch_array[:, :, 0])
+            #
+            # print(x_sharding.addressable_devices)
+            # print(state_sharding)
+            # jax.debug.visualize_array_sharding(params['Dense_0']['kernel'])
+            # print(global_batch_array.shape)
+            print(end - start)
+
+
+            print(grad)
+
+
+
+
+
+    return grad
+
+
+
+
+
+
+
+
 def case2():
     class DPDense(nn.Module):
         dim: int
@@ -162,11 +281,6 @@ def case2():
 
     train_step_pmap = jax.pmap(train_step, axis_name='batch')
 
-    #
-    # start = time.time()
-    # print(abs(global_batch_array[-1]))
-    # end = time.time()
-    # print(end - start)
 
     global_batch_array = shard(x)
     params = replicate(params)
@@ -197,9 +311,12 @@ if __name__ == "__main__":
     if jax.process_index() == 0:
         print(jax.devices())
 
-    out1 = case1()
-    out2 = case2()
 
-    out2 = out2.reshape(out1.shape)
+    out3=case3()
 
-    print(out1 - out2)
+    # out1 = case1()
+    # out2 = case2()
+    #
+    # out2 = out2.reshape(out1.shape)
+    #
+    # print(out1 - out2)
